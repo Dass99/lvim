d639efe197f6eb0163cc5ff60c7d793bd50cc98a
Douglas <douglas.s.santos1603@gmail.com>
2024-01-19 19:55:51 -0300

    first commit

---
+++ lua/dg/colorscheme.lua
@@ -0,0 +1,7 @@
+lvim.plugins =  {
+  -- "LunarVim/primer.nvim",
+  "LunarVim/darkplus.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+}
+lvim.colorscheme = "darkplus"
